463f9d0cfba0f4c61451f1f99783adcc0ee8ac01
jpuljko <jxp5761@rit.edu>
2021-12-09 23:07:29 +0100

    Initial Commit

---
+++ IKEAStore/IKEAStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  IKEAStoreApp.swift
+//  IKEAStore
+//
+//  Created by Josipa Puljko on 09.12.2021..
+//
+
+import SwiftUI
+
+@main
+struct IKEAStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
