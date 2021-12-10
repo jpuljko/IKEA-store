c38f46210f2a7cebadf662e3c60c9ba60e69ef29
jpuljko <jxp5761@rit.edu>
2021-12-10 10:29:55 +0100

    Added NavigationBarView to ContentView and aligned it with the footer

--- IKEAStore/ContentView.swift
+++ IKEAStore/ContentView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }
+        }
+        .ignoresSafeArea(.all, edges: .top)
+//        TODO: FIX BACKGROUND - TURN IT INTO A LIGHT GRAY
+//        .background(Color.gray.ignoresSafeArea(.all, edges: .all))
     }
 }
 
