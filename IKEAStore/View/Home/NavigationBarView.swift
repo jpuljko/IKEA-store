1283fa1db0b06737e69198e1df5b2da693c31da2
jpuljko <jxp5761@rit.edu>
2021-12-10 10:11:04 +0100

    Created NavigationBarView

---
+++ IKEAStore/View/Home/NavigationBarView.swift
@@ -0,0 +1,43 @@
+//
+//  NavigationBarView.swift
+//  IKEAStore
+//
+//  Created by Josipa Puljko on 10.12.2021..
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            })
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
+            })
+        }
+    }
+}
+
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
