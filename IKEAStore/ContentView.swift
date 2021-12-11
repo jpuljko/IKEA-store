128ff0b1bcbd4088a8559e839272e8918dacd4d7
jpuljko <jxp5761@rit.edu>
2021-12-11 17:32:58 +0100

    Added chair component, works perfectly

--- IKEAStore/ContentView.swift
+++ IKEAStore/ContentView.swift
@@ -11,27 +11,36 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            VStack(spacing: 0) {
-                NavigationBarView()
-                    .padding(.horizontal, 15)
-                    .padding(.bottom)
-                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
-                    .background(Color.white)
-                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
-                
-                CategoryGridView()
-                
-                TitleView(title: "CHAIRS")
-                
-                Spacer()
-                
-                FooterView()
-                    .padding(.horizontal)
-            }
+            ScrollView(.vertical, showsIndicators: false, content: {
+                VStack(spacing: 0) {
+                    NavigationBarView()
+                        .padding(.horizontal, 15)
+                        .padding(.bottom)
+                        .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                        .background(Color.white)
+                        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                    
+                    CategoryGridView()
+                    
+                    TitleView(title: "Chairs")
+                    
+                    LazyVGrid(columns: gridLayout, spacing: 15, content: {
+                        ForEach(products) { product in
+                            ProductView(product: product)
+                        }
+                    })
+                        .padding(15)
+                    
+                    Spacer()
+                    
+                    FooterView()
+                        .padding(.horizontal)
+                }
+            })
         }
         .ignoresSafeArea(.all, edges: .top)
 //        TODO: FIX BACKGROUND - TURN IT INTO A LIGHT GRAY
-//        .background(Color.gray.ignoresSafeArea(.all, edges: .all))
+        .background(bgColor.ignoresSafeArea(.all, edges: .all))
     }
 }
 
--- IKEAStore/View/Home/ProductView.swift
+++ IKEAStore/View/Home/ProductView.swift
@@ -9,35 +9,37 @@ import SwiftUI
 
 struct ProductView: View {
     
-    let product: Product
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 6, content: {
-//            ZStack {
-//                Image(product.image)
-//                    .resizable()
-//                    .scaledToFit()
-//                    .padding(10)
-//            }
-//            .background(Color(red: product.color[0], green: product.color[1], blue: product.color[3]))
-//            .cornerRadius(12)
-            
-            Text(product.name)
-                .font(.title3)
-                .fontWeight(.black)
-            
-            Text("$\(product.price)")
-                .fontWeight(.semibold)
-                .foregroundColor(.gray)
-        })
-    }
+  let product: Product
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 6, content: {
+
+      ZStack {
+        Image(product.image)
+          .resizable()
+          .scaledToFit()
+          .padding(10)
+      }
+      .background(Color(red: product.color[0], green: product.color[1], blue: product.color[2]))
+      .cornerRadius(12)
+
+      Text(product.name)
+        .font(.title3)
+        .fontWeight(.black)
+
+      Text("$\(product.price)")
+        .fontWeight(.semibold)
+        .foregroundColor(.gray)
+    })
+  }
 }
 
-struct ProductView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProductView(product: products[0])
-            .previewLayout(.fixed(width: 200, height: 300))
-            .padding()
-            .background(bgColor)
-    }
+
+struct ProductItemView_Previews: PreviewProvider {
+  static var previews: some View {
+    ProductView(product: products[0])
+      .previewLayout(.fixed(width: 200, height: 300))
+      .padding()
+      .background(bgColor)
+  }
 }
